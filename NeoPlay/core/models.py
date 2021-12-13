227540e425bce898f0b44ed54fb82ff231bab78d
Udalovvv7 <udal.nik@mail.ru>
2021-12-13 11:51:46 +0300

    Models init

--- NeoPlay/core/models.py
+++ NeoPlay/core/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+
+class UserModel (models.Model):
+    id: models.PositiveIntegerField(unique=True, primary_key=True)
+    user_name: models.CharField(max_length=50, unique=True)
+    name: models.CharField(max_length=50)
+    surname: models.CharField(max_length=50, blank=True)
+    email: models.EmailField(unique=True)
+    password: models.CharField(max_length=30)
+
+
+class EventModel (models.Model):
+    id: models.PositiveIntegerField(unique=True, primary_key=True)
+    name: models.CharField(max_length=150, unique=True)
+    game_id: models.PositiveSmallIntegerField()
+    date: models.DateTimeField()
+    author_id: UserModel.id()
+    description: models.TextField(max_length=2000)
+    status: models.PositiveSmallIntegerField()
+    recording: models.URLField()
+
+
+class UserEvent (models.Model):
+    user_id: UserModel.id
+    event_id: EventModel.id
